198cf4e3952fe88a834067077607da5afd3a95e5
hviswanathan <hviswanathan@TMZ-DPC-2289.ardianet.net>
2023-09-26 18:39:47 +0530

    Maven Java Project

---
+++ demokit/src/main/java/demokit/demokit/App.java
@@ -0,0 +1,13 @@
+package demokit.demokit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
